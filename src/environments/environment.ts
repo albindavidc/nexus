2b97ad57e31247559faa29b759deed5b8a0e387e
Albin David C <albindavidc007@gmail.com>
2026-06-20 18:47:05 +0530

    feat(notification): updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1'
+  apiUrl: 'http://localhost:3000/api/v1',
+  vapidPublicKey:
+    'BGKyTk0N_uGnrgXDOQFik25x8aTGAUi1n1tasnqaTELr6G9p4OoQF1ZAAZp4HKJzV7KN0262EWVQ5ctmiHYlSAo',
 };
